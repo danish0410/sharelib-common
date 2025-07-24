9c9cf20a6cab0a4d753de440bcbe1a5830a07f4d
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-07-24 12:47:50 +0530

    adding the files

---
+++ vars/src/org/commonlib/BuildUtils.groovy
@@ -0,0 +1,29 @@
+package org.commonlib
+
+class BuildUtils implements Serializable {
+    def steps
+
+    BuildUtils(steps = null) {
+        this.steps = steps ?: this
+    }
+
+    def build(Map config) {
+        def appType = config.appType?.toLowerCase()
+        switch(appType) {
+            case 'springboot':
+                steps.echo "Building Spring Boot App: ${config.appName}"
+                steps.sh "mvn clean package -f ${config.pathToPom ?: '.'}"
+                break
+            case 'nginx':
+                steps.echo "Building Nginx App: ${config.appName}"
+                steps.sh "docker build -t ${config.appName}:latest ${config.context ?: '.'}"
+                break
+            case 'php':
+                steps.echo "Deploying PHP App: ${config.appName}"
+                steps.sh "cp -r ${config.source ?: '.'} /var/www/html/${config.appName}"
+                break
+            default:
+                steps.error "Unsupported appType: $appType"
+        }
+    }
+}
